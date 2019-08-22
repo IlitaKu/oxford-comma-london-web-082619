ccc47dbab4be108bfd5f3f57646f6cd27c220a4f
Ilita Kunelyte <kunelyte.ilita90@gmail.com>
2019-08-22 16:40:06 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,10 @@
 def oxford_comma(array)
-if array.length == 1
-  array.join("")
-  elsif array.length == 2
-    array.join(" and ")
-    elsif array.length == 3
-    array.split(",")
-    array.insert(2, "and")
+if array.length == 2
+  array[-2] << "and"
+  elsif array.length == 1
+    array.join
+    elsif array.length > 2
+    array[-1].pretend "and"
     array.join(" ")
-    
   end
   end
