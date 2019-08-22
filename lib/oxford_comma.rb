bdc4b1c5d31d300669482118c6de4adf08c22ea2
Ilita Kunelyte <kunelyte.ilita90@gmail.com>
2019-08-22 16:25:00 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,9 +4,9 @@ if array.length == 1
   elsif array.length == 2
     array.join(" and ")
     elsif array.length == 3
-    array.insert(2, "and")
-    array.insert(0, ",")
+    array.insert(3, "and")
     array.insert(1, ",")
+    array.insert(2, ",")
     array.join(' ')
     
   end
